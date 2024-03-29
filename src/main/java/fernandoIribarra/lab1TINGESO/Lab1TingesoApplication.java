7f40047c873cb65c3bea1f575385b75522c99b49
Fernando Iribarra <fernando.iribarra@usach.cl>
2024-03-28 23:23:07 -0300

    Commit Inicial, spring boot setup

---
+++ src/main/java/fernandoIribarra/lab1TINGESO/Lab1TingesoApplication.java
@@ -0,0 +1,13 @@
+package fernandoIribarra.lab1TINGESO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1TingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1TingesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fernandoIribarra/lab1TINGESO/Lab1TingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package fernandoIribarra.lab1TINGESO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1TingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
