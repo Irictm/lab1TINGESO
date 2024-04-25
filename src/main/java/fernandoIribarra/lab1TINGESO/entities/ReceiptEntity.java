08c47893d79f7759fb2276c42ca6ebaa2b5b6b96
Fernando Iribarra <fernando.iribarra@usach.cl>
2024-04-24 23:08:15 -0400

    Se corrigen nombres erroneos y se inicia la reimplementacion de "Receip".

--- src/main/java/fernandoIribarra/lab1TINGESO/entities/ReceiptEntity.java
+++ src/main/java/fernandoIribarra/lab1TINGESO/entities/ReceiptEntity.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "receipt")
+@Table(name = "receipts")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/fernandoIribarra/lab1TINGESO/entities/RepairEntity.java
+++ src/main/java/fernandoIribarra/lab1TINGESO/entities/RepairEntity.java
@@ -28,4 +28,5 @@ public class RepairEntity {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateOfPickUp;
     private long id_vehicle;
+    private long id_receipt;
 }
